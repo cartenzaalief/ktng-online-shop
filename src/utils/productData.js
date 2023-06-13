5d3dd297622c8fb7120919a1f733d16dc05aa2b6
cartenzaalief <cartenzaalief@gmail.com>
2023-06-13 19:59:37 +0700

    add product data

---
+++ src/utils/productData.js
@@ -0,0 +1,88 @@
+import esseChangeApplemint from "../assets/IMGproducts/Change_Family/ESSE_Change_Applemint.png"
+import esseChangeDouble from "../assets/IMGproducts/Change_Family/ESSE_Change_Double.png"
+import esseChangeGrape from "../assets/IMGproducts/Change_Family/ESSE_Change_Grape.png"
+import esseChangeHimalaya from "../assets/IMGproducts/Change_Family/ESSE_Change_Himalaya.png"
+import esseChangeJuicy from "../assets/IMGproducts/Change_Family/ESSE_Change_Juicy.png"
+import esseBanaPop from "../assets/IMGproducts/POP_Family/ESSE_Bana_POP.jpg"
+import esseBerryPop12 from "../assets/IMGproducts/POP_Family/ESSE_Berry_POP_12.png"
+import esseBerryPop16 from "../assets/IMGproducts/POP_Family/ESSE_Berry_POP_16.png"
+import esseDoublePop from "../assets/IMGproducts/POP_Family/ESSE_Double_POP.png"
+import esseHoneyPop from "../assets/IMGproducts/POP_Family/ESSE_Honey_POP.png"
+import essePunchPop from "../assets/IMGproducts/POP_Family/ESSE_Punch_POP.jpg"
+import esseShufflePop from "../assets/IMGproducts/POP_Family/ESSE_Shuffle_POP.png"
+import juaraFilterPrime from "../assets/IMGproducts/Juara_Family/Juara_Filter_Prime.png"
+import juaraTehManisFilter from "../assets/IMGproducts/Juara_Family/Juara_Filter.jpg"
+import juaraTehManisKretek from "../assets/IMGproducts/Juara_Family/Juara_Teh_Manis.png"
+import juaraJambuKretek from "../assets/IMGproducts/Juara_Family/Juara_Jambu.png"
+import juaraManggaKretek from "../assets/IMGproducts/Juara_Family/Juara_Mangga.png"
+import juaraKopiKretek from "../assets/IMGproducts/Juara_Family/Juara_Kopi.png"
+import juaraMasterKretek from "../assets/IMGproducts/Juara_Family/Juara_Master.png"
+import winFilter12 from "../assets/IMGproducts/WIN_Family/WIN_Filter_12.jpg"
+import winFilter20 from "../assets/IMGproducts/WIN_Family/WIN_Filter.png"
+import winBold from "../assets/IMGproducts/WIN_Family/WIN_Bold.jpeg"
+import winClick from "../assets/IMGproducts/WIN_Family/WIN_Click_20.png"
+import winKretek from "../assets/IMGproducts/WIN_Family/WIN_Kretek.png"
+import winKretekHijau from "../assets/IMGproducts/WIN_Family/WIN_Kretek_Hijau.png"
+import winKretekTehManis from "../assets/IMGproducts/WIN_Family/WIN_Kretek_Teh_Manis.png"
+import bohemMojito from "../assets/IMGproducts/SPM_Family/Bohem_Mojito.jpg"
+import esseBlue from "../assets/IMGproducts/SPM_Family/ESSE_Blue.png"
+import esseBlueChange from "../assets/IMGproducts/SPM_Family/ESSE_Blue_Change.png"
+import esseGoldenLeaf from "../assets/IMGproducts/SPM_Family/ESSE_Golden_Leaf.png"
+import gtRed from "../assets/IMGproducts/SPM_Family/GT_Red.jpg"
+import raisonBlueCat from "../assets/IMGproducts/SPM_Family/RAISON_Blue_Cat.png"
+import nBold from "../assets/IMGproducts/N_Family/N_Bold.png"
+import nKretek from "../assets/IMGproducts/N_Family/N_Kretek.png"
+import pensilMasInternational16 from "../assets/IMGproducts/PENSIL_MAS/Pensil_Mas_International_16.jpg"
+import pensilMasInternational20 from "../assets/IMGproducts/PENSIL_MAS/Pensil_Mas_International_20.jpg"
+import pensilMas12 from "../assets/IMGproducts/PENSIL_MAS/Pensil_Mas_12.jpg"
+import pensilMasMild from "../assets/IMGproducts/PENSIL_MAS/Pensil_Mas_Mild.jpg"
+import pensilMasBold from "../assets/IMGproducts/PENSIL_MAS/Pensil_Mas_Bold.png"
+import bheta from "../assets/IMGproducts/SKT/BHETA.png"
+import lintangEnam from "../assets/IMGproducts/SKT/Lintang_Enam.png"
+
+export const productsData = [
+    { name: "ESSE CHANGE Applemint 16s", price: 26700, image: esseChangeApplemint },
+    { name: "ESSE CHANGE Applemint 20s", price: 32700, image: esseChangeApplemint },
+    { name: "ESSE CHANGE Double", price: 34500, image: esseChangeDouble },
+    { name: "ESSE CHANGE Grape 16s", price: 26700, image: esseChangeGrape },
+    { name: "ESSE CHANGE Grape 20s", price: 32700, image: esseChangeGrape },
+    { name: "ESSE CHANGE Himalaya", price: 34500, image: esseChangeHimalaya },
+    { name: "ESSE CHANGE Juicy 16s", price: 26700, image: esseChangeJuicy },
+    { name: "ESSE CHANGE Juicy 20s", price: 32700, image: esseChangeJuicy },
+    { name: "ESSE Bana POP", price: 20400, image: esseBanaPop },
+    { name: "ESSE Berry POP 12s", price: 20400, image: esseBerryPop12 },
+    { name: "ESSE Berry POP 16s", price: 27700, image: esseBerryPop16 },
+    { name: "ESSE Double POP", price: 27700, image: esseDoublePop },
+    { name: "ESSE Honey POP", price: 27700, image: esseHoneyPop },
+    { name: "ESSE Punch POP", price: 27700, image: essePunchPop },
+    { name: "ESSE Shuffle POP", price: 27700, image: esseShufflePop },
+    { name: "JUARA Filter Prime", price: 20300, image: juaraFilterPrime },
+    { name: "JUARA Teh Manis Filter", price: 20300, image: juaraTehManisFilter },
+    { name: "JUARA Teh Manis Kretek", price: 12600, image: juaraTehManisKretek },
+    { name: "JUARA Jambu Kretek", price: 12600, image: juaraJambuKretek },
+    { name: "JUARA Mangga Kretek", price: 12600, image: juaraManggaKretek },
+    { name: "JUARA Kopi Kretek", price: 12600, image: juaraKopiKretek },
+    { name: "JUARA Master Kretek", price: 12600, image: juaraMasterKretek },
+    { name: "WIN Filter 12s", price: 14800, image: winFilter12 },
+    { name: "WIN Filter 20s", price: 20800, image: winFilter20 },
+    { name: "WIN Bold", price: 20800, image: winBold },
+    { name: "WIN Click", price: 20800, image: winClick },
+    { name: "WIN Kretek", price: 7600, image: winKretek },
+    { name: "WIN Kretek Hijau", price: 7600, image: winKretekHijau },
+    { name: "WIN Kretek Teh Manis", price: 7600, image: winKretekTehManis },
+    { name: "BOHEM CIGAR Mojito", price: 33000, image: bohemMojito },
+    { name: "ESSE Blue", price: 34300, image: esseBlue },
+    { name: "ESSE Blue Change", price: 34500, image: esseBlueChange },
+    { name: "ESSE Golden Leaf", price: 37300, image: esseGoldenLeaf },
+    { name: "GT Red", price: 22200, image: gtRed },
+    { name: "RAISON Blue Cat", price: 33000, image: raisonBlueCat },
+    { name: "N Bold", price: 22800, image: nBold },
+    { name: "N Kretek", price: 12600, image: nKretek },
+    { name: "PENSIL MAS International 16s", price: 17800, image: pensilMasInternational16 },
+    { name: "PENSIL MAS International 20s", price: 22800, image: pensilMasInternational20 },
+    { name: "PENSIL MAS 12", price: 14800, image: pensilMas12 },
+    { name: "PENSIL MAS Mild", price: 22800, image: pensilMasMild },
+    { name: "PENSIL MAS Bold", price: 22800, image: pensilMasBold },
+    { name: "BHETA", price: 12600, image: bheta },
+    { name: "LINTANG ENAM", price: 12600, image: lintangEnam },
+]
